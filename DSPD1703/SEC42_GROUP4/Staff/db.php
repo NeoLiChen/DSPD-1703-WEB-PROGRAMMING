e0643f94e39835003e9596364cfdd41571cac494
NeoLiChen <lichenneo@gmail.com>
2026-06-13 13:26:39 +0800

    Delete DSPD1703 directory

--- DSPD1703/SEC42_GROUP4/Staff/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $serverName = 'localhost';
-    $userName = 'g04sec42';
-    $passWord = 'g04s42stsdb';
-    $dataBase = 'STS42';
-
-    $conn = mysqli_connect($serverName, $userName, $passWord, $dataBase);
-?>
